c01b6b4fc21fd2d875754632eedea21d89e2adb9
zozen <zozen@gmail.com>
2017-12-12 15:09:06 +0700

    ee

--- prefin_04.py
+++ prefin_04.py
@@ -8,7 +8,7 @@ x = x.replace(' ','')
 for i in x:
     if i not in ls:
         s += i
-        # print(i,end='')
+        # print(i)
     ls.append(i)
 # print(s)
 if len(code) == len(s):
--- prefin_08.py
+++ prefin_08.py
@@ -1,24 +1,48 @@
+# num = int(input())
+# ls = []
+# lsrm = []
+# for i in range(num):
+#     x = int(input())
+#     if x not in ls:
+#         if x > 0:
+#             ls.append(x)
+# ls.sort()
+# ans = []
+# for i in range(len(ls)-1):
+#     a = ls[i + 1] - ls[i]
+#     ans.append(a)
+# # print(ls)
+# # print(ans)
+# c = ans.count(min(ans))
+# for i in range(c):
+#     minn = min(ans)
+#     # print("minn =",minn)
+#     index = ans.index(minn)
+#     # print("index =",index)
+#     print("%d %d"%(ls[index],ls[index]+minn))
+#     ans.pop(index)
+#     ls.pop(index)
+
+
+
 num = int(input())
+c = 0
 ls = []
-lsrm = []
 for i in range(num):
     x = int(input())
     if x not in ls:
-        if x > 0:
-            ls.append(x)
+        ls.append(x)
+print(ls)
 ls.sort()
-ans = []
+div = []
 for i in range(len(ls)-1):
-    a = ls[i + 1] - ls[i]
-    ans.append(a)
-# print(ls)
-# print(ans)
-c = ans.count(min(ans))
-for i in range(c):
-    minn = min(ans)
-    # print("minn =",minn)
-    index = ans.index(minn)
-    # print("index =",index)
-    print("%d %d"%(ls[index],ls[index]+minn))
-    ans.pop(index)
+    d = ls[i+1] - ls[i]
+    div.append(d)
+print(div)
+minn = min(div)
+count = div.count(minn)
+for i in range(count):
+    index = div.index(minn)
+    print("%d %d"%(ls[index],ls[index] + minn))
     ls.pop(index)
+    div.pop(index)
